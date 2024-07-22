9fec0b21ee7ddf54ba2af9a4138eba7a7bfab7fb
Pierce Morris <pierce.morris1998@gmail.com>
2024-07-22 17:52:00 +0900

    add menu for different options

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,40 @@
-import MapChart from '~/components/MapChart'
+enum quizType {
+  regionName,
+  regionLocation,
+  regionReading,
+}
 
 export default function Home() {
+  const quizOptions = [
+    {
+      key: quizType.regionName,
+      title: "Name the prefecture",
+      desc: "Type the name of the prefecture that is highlighted",
+    },
+    {
+      key: quizType.regionLocation,
+      title: "Select the prefecture",
+      desc: "Find the prefecture on the map",
+    },
+    {
+      key: quizType.regionReading,
+      title: "Read the prefecture",
+      desc: "Type the reading of the prefecture",
+    },
+  ]
   return (
     <main>
-      <MapChart />
+      <div className="flex h-screen flex-col items-center justify-center gap-5">
+        {quizOptions.map((option) => (
+          <div
+            key={option.key}
+            className="flex w-1/3 flex-col rounded-md border border-slate-300 px-4 py-2 hover:bg-fuchsia-600"
+          >
+            <span className="text-xl font-bold">{option.title}</span>
+            <span className="text-sm">{option.desc}</span>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
